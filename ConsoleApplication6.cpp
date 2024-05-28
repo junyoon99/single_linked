43fd8d3d3215f9747d30de2cc213774fdc66a10e
Hyeontae An <83248120+bearbig-12@users.noreply.github.com>
2024-05-28 17:58:12 +0900

    초기 커밋.

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+struct Bullet 
+{
+	int damage;
+	float speed;
+	float size;
+
+	Bullet *pNext;
+};
+
+int main()
+{
+	Bullet *a = new Bullet;
+	Bullet *b = new Bullet;
+
+	a->pNext = b;
+}
